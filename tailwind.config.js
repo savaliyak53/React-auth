ab9fe9627608381c1b12fc98b30b480cfc1b180a
Kishan savaliya <kishan1054@gmail.com>
2023-08-31 20:42:56 +0530

    feat: add custom gradient
    Two cuustom gradient added 1:gradient-color 2:gradient-image
    
    ref:#savaliyak53
    
    :wq

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,32 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        gradient0: "url(/public/image/gradient.jpeg)",
+        gradient1: "linear-gradient(135deg, #83A5F2 0%, #204ECF 58.85%)",
+      },
+      colors: {
+        "regal-blue": "#243c5a",
+        primary: {
+          delft: {
+            dark: "#204ECF",
+            medium: "rgba(32, 78, 207, 0.90)",
+            light: "rgba(32, 78, 207, 0.70)",
+          },
+          watermelons: {
+            dark: "#F26749",
+            medium: "rgba(242, 103, 73, 0.50)",
+            light: "rgba(242, 103, 73, 0.30)",
+          },
+          cornflower: {
+            dark: "#83A5F2",
+            medium: "rgba(131, 165, 242, 0.50)",
+            light: "rgba(131, 165, 242, 0.30)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
