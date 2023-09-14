6df4883f81389fc0548cb6e6b4a077f3da1ead47
kishan savaliya <kishan1054@gmail.com>
2023-09-14 12:51:26 +0530

    chores/using_setData_access_state_data

--- src/containers/registration.js
+++ src/containers/registration.js
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Registration = () => {
+  const [data, setData] = useState([
+    {
+      color: "purple",
+      type: "minivan",
+      registration: new Date("2012-02-03"),
+      capacity: 7,
+    },
+    {
+      color: "blue",
+      type: "maxivan",
+      registration: new Date("2012-02-03"),
+      capacity: 7,
+    },
+  ]);
+
+  const handleData = () => {
+    setData((current) => {
+      current.map((curr) => console.log(curr, "current"));
+    });
+  };
+
   return (
-    <div className="bg-gradient0 h-screen">
+    <button onClick={handleData}>
       <div>Registration</div>
-    </div>
+    </button>
   );
 };
 
